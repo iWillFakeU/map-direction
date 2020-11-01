74ca504f9a71abc836413275f6aece1b7133ef27
José Santos <jose.santos@itpeople.pt>
2020-11-01 23:02:02 +0000

    udpate

--- src/index.jsx
+++ src/index.jsx
@@ -9,22 +9,6 @@ export const render = () => {
   if (process.env.NODE_ENV !== 'development') {
     const isNewWindguru = window.location.hostname.includes('www.windguru.cz')
 
-    if (isNewWindguru) {
-      const forecastPageContent = document.getElementById('forecasts-page-content')
-
-      console.log('Removing advertisement div...')
-      forecastPageContent.removeChild(forecastPageContent.childNodes[1])
-
-      const rootDiv = document.createElement('div')
-      rootDiv.setAttribute('id', 'root')
-      forecastPageContent.insertBefore(rootDiv, forecastPageContent.children[1])
-    } else {
-      const forecastPageContent = document.getElementById('spot_search_res')
-
-      forecastPageContent.setAttribute('id', 'root')
-      forecastPageContent.removeChild(forecastPageContent.childNodes[0])
-    }
-
     const scriptReact = document.createElement('script')
     scriptReact.setAttribute('src', 'https://unpkg.com/react@17/umd/react.development.js')
     document.head.appendChild(scriptReact)
@@ -37,7 +21,35 @@ export const render = () => {
     scriptMap.setAttribute('href', 'https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css')
     scriptMap.setAttribute('rel', 'stylesheet')
     document.head.appendChild(scriptMap)
-  }
 
-  ReactDOM.render(<App />, document.getElementById('root'))
+    if (isNewWindguru) {
+      const forecastPageContent = document.getElementById('forecasts-page-content')
+
+      console.log('Removing advertisement div...')
+      forecastPageContent.removeChild(forecastPageContent.childNodes[1])
+
+      const rootDiv = document.createElement('div')
+      rootDiv.setAttribute('id', 'root')
+      forecastPageContent.insertBefore(rootDiv, forecastPageContent.children[1])
+
+      ReactDOM.render(<App />, document.getElementById('root'))
+    } else {
+      const waitForElementToBePresent = setInterval(() => {
+        console.log('waiting for element to be present...')
+
+        if (document.getElementById('spot_search_res')) {
+          clearInterval(waitForElementToBePresent)
+
+          const forecastPageContent = document.getElementById('spot_search_res')
+
+          forecastPageContent.setAttribute('id', 'root')
+          forecastPageContent.removeChild(forecastPageContent.childNodes[0])
+
+          ReactDOM.render(<App />, document.getElementById('root'))
+        }
+      }, 500)
+    }
+  } else {
+    ReactDOM.render(<App />, document.getElementById('root'))
+  }
 }
